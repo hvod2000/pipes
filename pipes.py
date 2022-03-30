63936dae4514244d344c364671655bbeb149d7b5
Uladzislau Khamkou <kham@tuta.io>
2022-03-30 23:20:59 +0300

    Support syntax: pipe2 = pipe1(keywordarg=something)

--- pipes.py
+++ pipes.py
@@ -4,10 +4,14 @@ from functools import update_wrapper
 class Pipe:
     def __init__(self, function, kwargs=None):
         self.function = function
+        self.kwargs = kwargs or {}
         update_wrapper(self, function)
 
     def __call__(self, *args, **kwargs):
-        return Pipe(self.function, *args, **kwargs)
+        kwargs = kwargs | self.kwargs
+        if len(args):
+            return args | Pipe(self.function, kwargs)
+        return Pipe(self.function, kwargs)
 
     def __ror__(self, other):
-        return self.function(other)
+        return self.function(other, **self.kwargs)
