9ddea1d2f9dfbe6dbe729f0613e62fb5177ea56d
Uladzislau Khamkou <kham@tuta.io>
2022-03-31 10:56:45 +0300

    Support partial application of a pipe

--- pipes.py
+++ pipes.py
@@ -2,16 +2,23 @@ from functools import update_wrapper
 
 
 class Pipe:
-    def __init__(self, function, kwargs=None):
+    def __init__(self, function, args_len=1, args_preproc=None, args=None):
         self.function = function
-        self.kwargs = kwargs or {}
+        self.args_len = args_len
+        self.args_preprocessing = args_preproc
+        self.args, self.kwargs = ((), {}) if args is None else args
         update_wrapper(self, function)
 
     def __call__(self, *args, **kwargs):
-        kwargs = kwargs | self.kwargs
-        if len(args):
-            return args | Pipe(self.function, kwargs)
-        return Pipe(self.function, kwargs)
+        preproc = self.args_preprocessing
+        kwargs = self.kwargs | kwargs
+        if len(args) + len(self.args) >= self.args_len:
+            args = (args[0],) + self.args + args[1:]
+            if self.args_preprocessing is not None:
+                args = self.args_preprocessing(args)
+            return self.function(*args, **kwargs)
+        args = (self.args + args, kwargs)
+        return Pipe(self.function, self.args_len, preproc, args)
 
     def __ror__(self, other):
-        return self.function(other, **self.kwargs)
+        return self(other)
