ef98e053efd69880ea2dee0df16c72c35776d052
Uladzislau Khamkou <kham@tuta.io>
2022-04-02 11:45:33 +0300

    implement dot syntax for function calling

--- myiters.py
+++ myiters.py
@@ -11,5 +11,11 @@ class Iter:
     def map(self, f):
         return Iter(map(f, self.iterator))
 
+    def __getattr__(self, name):
+        frame = inspect.currentframe()
+        f = frame.f_back.f_locals.get(name, None)
+        del frame
+        return lambda *args, **kwargs: Iter(f(self.iterator, *args, **kwargs))
+
     def __iter__(self):
         yield from self.iterator
