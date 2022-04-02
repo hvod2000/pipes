d32c22a69a96bc3894e7abd24e303b3e321f431d
Uladzislau Khamkou <kham@tuta.io>
2022-04-02 19:16:26 +0300

    put the whole itertools into methods of Iter

--- myiters.py
+++ myiters.py
@@ -1,4 +1,6 @@
 import inspect
+import itertools
+import myitertools
 
 
 class Iter:
@@ -22,6 +24,30 @@ class Iter:
 
     def __iter__(self):
         yield from self.iterator
+
+
+def generator(f):
+    return lambda *args, **kwargs: Iter(f(*args, **kwargs))
+
+
+# itertools functionality
+Iter.accumulate = generator(itertools.accumulate)
+Iter.chain = generator(myitertools.chain)
+Iter.combinations = generator(itertools.combinations)
+Iter.combinations_with_repetition = generator(itertools.combinations_with_replacement)
+Iter.combinations_with_replacement = generator(itertools.combinations_with_replacement)
+Iter.compress = generator(itertools.compress)
+Iter.cycle = generator(itertools.cycle)
+Iter.skip_while = generator(myitertools.skip_while)
+Iter.skip_if = generator(myitertools.skip_if)
+Iter.group_by = generator(itertools.groupby)
+Iter.slice = generator(itertools.islice)
+Iter.permutations = generator(itertools.permutations)
+Iter.permutations_with_repetition = generator(itertools.product)
+Iter.product = generator(itertools.product)
+Iter.take_while = generator(itertools.takewhile)
+Iter.tee = generator(itertools.tee)
+Iter.zip_longest = generator(itertools.zip_longest)
 
 
 if __name__ == "__main__":
